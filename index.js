c984b4e5a784a4d7cf18672a1e991ee74b31a6a1
ptysell <9489667+ptysell@users.noreply.github.com>
2022-11-12 11:21:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -134,11 +134,12 @@ function VirturalDeviceAccessoryAction(log, name, accessory) {
 	
   this.log = log;
   this.name = name + " Accessory Action";
-  this.targetaccessory = accessory;
   this._service = new Service.Switch(this.name);
   this._state = false;
   
-  this.log("TEST REGISTRATION ACCESSORY ACTION PARENT : " + targetaccessory.name);
+  this.accessory = accessory;
+  
+  this.log("TEST REGISTRATION ACCESSORY ACTION PARENT : " + accessory.name);
     
   
   this.informationService = new Service.AccessoryInformation();
