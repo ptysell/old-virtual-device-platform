d20f30e30f1767eccebaa3def1daf99cb1c24fae
ptysell <9489667+ptysell@users.noreply.github.com>
2022-11-12 10:39:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ VirturalDevicePlatform.prototype = {
 
 function VirturalDeviceAccessory(log, device) {
 	this.log = log;
-	this.name = device["name"],
+	this.name = device["name"];
+	
+	this.accessoryaction = new VirturalDeviceAccessoryAction(this.log, this.name);
 	
 	console.log(this.name + " = " + "Test");
 	
@@ -85,5 +87,72 @@ VirturalDeviceAccessory.prototype.setCurrentValue = function(value, callback) {
 VirturalDeviceAccessory.prototype.getServices = function() {
 	this.log.info("Get Services Function");
 	return this.services;
+}
+
+
+
+
+
+function VirturalDeviceAccessoryAction(log, name) {
+	
+  this.log = log;
+  this.name = name = " Accessory Action";
+  this._service = new Service.Switch(this.name);
+  this._state = false;
+    
+  
+  this.informationService = new Service.AccessoryInformation();
+  this.informationService
+      .setCharacteristic(Characteristic.Manufacturer, 'Virtual Device Platform')
+      .setCharacteristic(Characteristic.Model, 'Accessory Action')
+      .setCharacteristic(Characteristic.FirmwareRevision, HomebridgeVDPVersion)
+      .setCharacteristic(Characteristic.SerialNumber, 'VDPAccessoryAction_' + this.name.replace(/\s/g, '_'));
+  
+  this.cacheDirectory = HomebridgeAPI.user.persistPath();
+  this.storage = require('node-persist');
+  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});	
+	
+	
+  this._service.getCharacteristic(Characteristic.On)
+    .on('set', this._setOn.bind(this));
+	
+  var cachedState = this.storage.getItemSync(this.name);
+  if((cachedState === undefined) || (cachedState === false)) {
+    this._service.setCharacteristic(Characteristic.On, false);
+    } 
+  else {
+    this._service.setCharacteristic(Characteristic.On, true);
+    }
+	
+}
+
+VirturalDeviceAccessoryAction.prototype.getServices = function() {
+	
+  return [this.informationService, this._service];
+	
+}
+
+VirturalDeviceAccessoryAction.prototype._setOn = function(on, callback) {
+
+  if (on && this._state){
+    this.log("Setting [Accessory Action] : " + this.name.replace(/\s/g, '_') + " from " + on + " to " + !on);
+    setTimeout(() => {
+      this._service.setCharacteristic(Characteristic.On, false)  
+      }, 100);
+	  
+    } 
+  else {
+    this._state = on;
+	      this.log("Setting [Accessory Action] : " + this.name.replace(/\s/g, '_') + " from " + on + " to " + !on);
+
+    this.storage.setItemSync(this.name, on);
+    }
+	
+  callback();
+	
+}
+
+VirturalDeviceAccessoryAction.prototype.getStringFromState = function (state) {
+  return state ? 'on' : 'off'
 }
 
